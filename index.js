6eb026509abae882d77eeee422c58d40fcc74958
Alejandro <alejandrofigueirasx@gmail.com>
2022-12-03 21:06:18 -0500

    Integrado express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.use('/', express.static('public'))
+
+app.listen(port, () => {
+  console.log(`GD Cuba Rank app listening on port ${port}`)
+})

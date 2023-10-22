2d9774b2a63c15a695b2e4001d50c5b27e245b5d
SrDyw <mdayniel642@gmail.com>
2023-10-22 01:57:13 -0400

    haKuba matata

--- src/app/api/dev/query/route.js
+++ src/app/api/dev/query/route.js
@@ -3,18 +3,19 @@ import { secureQuery } from "@/database/db.functions";
 import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
-  const body = await req.json();
-  // console.log(body);
-  
-  const queryResult = await secureQuery(body.query);
+  if (process.env.NODE_ENV === "development") {
+    const body = await req.json();
+    // console.log(body);
 
+    const queryResult = await secureQuery(body.query);
 
-  if (!queryResult.isError()) {
-    return NextResponse.json(queryResult, {
-      status: 200,
-    });
+    if (!queryResult.isError()) {
+      return NextResponse.json(queryResult, {
+        status: 200,
+      });
+    }
   }
   return NextResponse.json(queryResult.error, {
-    status: 500
+    status: 400,
   });
 };

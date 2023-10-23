0013a452f6ed17b9a4ec8073fce2d4c29a3072d1
SrDyw <mdayniel642@gmail.com>
2023-10-23 11:31:01 -0400

    rank route integrated

--- src/app/context/GlobalContext.jsx
+++ src/app/context/GlobalContext.jsx
@@ -10,12 +10,13 @@ export default function GlobalContextProvider({ children }) {
     username: undefined,
     accountID: undefined,
   });
+  const [appliactionLoading, setAppliactionLoading] = useState({ toast: null });
   let firstAssembly = false;
 
   useEffect(() => {
     async function auth() {
       if (firstAssembly) return;
-      const loading = notify('Cargando, espera un momento', 'loading');
+      setGlobaLoading(true);
       const apiResult = await apiRequest(config.apiURL + "auth/me");
 
       if (!apiResult.isError(false)) {
@@ -26,12 +27,24 @@ export default function GlobalContextProvider({ children }) {
             accountID: data.accountID,
           });
       }
-      notifyDismiss(loading);
+      setGlobaLoading(false);
     }
 
     auth();
     firstAssembly = true;
   }, []);
+
+  const setGlobaLoading = (loading) => {
+    if (loading && !appliactionLoading.toast) {
+      const loading = notify("Cargando, espera un momento", "loading");
+      setAppliactionLoading({ toast: loading });
+    }
+    if (!loading) {
+      notifyDismiss(appliactionLoading.toast);
+      setAppliactionLoading({ toast: null });
+    }
+  };
+
   return (
     <GlobalContext.Provider value={{ currentUser, setCurrentUser }}>
       {children}

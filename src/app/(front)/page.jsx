63eef3a9547aefdb73398a6be2f0afc6b22fbaaa
Alejandro <alejandrofigueirasx@gmail.com>
2023-12-15 13:55:39 -0500

    Limpia la Home Page

--- src/app/(front)/page.jsx
+++ src/app/(front)/page.jsx
@@ -1,23 +1,11 @@
 "use client";
 
 import { useContext } from "react";
-import { ModalContext } from "../context/ModalContext";
-import GDIcon from "@/robtop/iconkit/GDIcon";
 
 export default function Home() {
-  const { openModal } = useContext(ModalContext);
   return (
     <>
-      <button
-        onClick={async () => {
-          
-        }}
-      >
-        Test
-      </button>
-      {/* <GDIcon type="robot" iconNumber={25} imageStyles={{border: "2px solid red"}} c2={5} glow={true}/> */}
-      <GDIcon type="ship" iconNumber={28} imageStyles={{border: "2px solid red"}} c2={5} glow={true}/>
-      {/* <GDIcon type="cube" iconNumber={1} imageStyles={{border: "2px solid red"}} c2={6} glow={true}/> */}
+      <h1 className="text-center text-lg mt-8"> Home Page</h1>
     </>
   );
 }

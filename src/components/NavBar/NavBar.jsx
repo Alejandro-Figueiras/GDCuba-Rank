8aec7be36c95a1a9de813671df8a49fdddce1c19
Alejandro <alejandrofigueirasx@gmail.com>
2023-12-15 20:18:06 -0500

    Separa UserDropdown en un componente separado

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useContext } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -10,32 +9,16 @@ import {
   NavbarMenuItem,
 } from "@nextui-org/navbar";
 
-import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownSection,
-  DropdownItem,
-} from "@nextui-org/dropdown";
-
-import { User } from "@nextui-org/user";
-import { Button } from "@nextui-org/button";
-
 // Modals
 import { useDisclosure } from "@nextui-org/modal";
 
 import { Link } from "@nextui-org/link";
 import Login from "../Forms/Login";
 import SignUp from "../Forms/SignUp";
-import { GlobalContext } from "@/app/context/GlobalContext";
-import { apiRequest } from "@/libs/serverRequest";
-import config from "../../../config";
-import { notify, notifyDismiss } from "@/libs/toastNotifications";
-import { operationText } from "@/locales/siteText";
-import { ModalContext } from "@/app/context/ModalContext";
+
 import { useSesion } from "@/hooks/useSesion";
-import { useGDIcon } from "@/robtop/iconkit/useGDIcon";
 import { usePathname } from "next/navigation";
+import UserDropdown from "./UserDropdown";
 
 const NavLink = ({href, children}) => {
   const rutaActual = usePathname()
@@ -48,7 +31,6 @@ const NavLink = ({href, children}) => {
 
 export default () => {
   const {currentUser, logout} = useSesion();
-  const { openModal } = useContext(ModalContext);
 
   const {
     isOpen: isOpenLogin,
@@ -60,23 +42,6 @@ export default () => {
     onOpen: onOpenSignUp,
     onOpenChange: onOpenChangeSignUp,
   } = useDisclosure();
-
-  const handleLogout = async () => {
-    const apiResult = await apiRequest(config.apiURL + "logout");
-    if (apiResult.isError()) {
-      return notify(operationText.error, "error");
-    }
-    logout();
-  };
-
-  // Icon
-  const {icon: iconAvatar} = useGDIcon({
-    type: 'cube',
-    username: currentUser.username,
-    effectDeps: [currentUser.username]
-  })
-
-  const logged = currentUser.username != undefined
 
   return (
     <>
@@ -91,93 +56,7 @@ export default () => {
 
         <NavbarContent justify="end">
           <NavbarItem>
-            <Dropdown placement="bottom-end">
-              <DropdownTrigger>
-                <Button color="default" variant="flat">
-                  {currentUser.username && (<img src={iconAvatar} alt={currentUser.username} className="h-7"/>)}
-                  {currentUser.username ?? "Sin cuenta"}
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Profile menu"
-                disabledKeys={["profile"]}
-                className="p-3"
-                itemClasses={{
-                  base: [
-                    "rounded-md",
-                    "text-default-500",
-                    "transition-opacity",
-                    "data-[hover=true]:text-foreground",
-                    "data-[hover=true]:bg-default-100",
-                    "dark:data-[hover=true]:bg-default-50",
-                    "data-[selectable=true]:focus:bg-default-50",
-                    "data-[pressed=true]:opacity-70",
-                    "data-[focus-visible=true]:ring-default-500",
-                  ],
-                }}
-              >
-                <DropdownSection aria-label="Profile & Actions">
-                  <DropdownItem
-                    isReadOnly
-                    key="profile"
-                    className="h-14 gap-2 opacity-100"
-                    textValue="User"
-                  >
-                    <User
-                      name={currentUser.username == undefined ? "Invitado" : currentUser.username}
-                      description={currentUser.username == undefined ? "none" : currentUser.phone}
-                      classNames={{
-                        name: "text-default-600",
-                        description: "text-default-500",
-                      }}
-                      avatarProps={{
-                        size: "sm",
-                        radius: 'none',
-                        src: iconAvatar,
-                      }}
-                    />
-                  </DropdownItem>
-                  {
-                    !logged && (
-                      <DropdownItem key="login-btn" onPress={onOpenLogin}>
-                        Iniciar Sesión
-                      </DropdownItem>
-                    ) 
-                  }
-                  {
-                    // Esta doble porque da un error con nextui
-                    !logged && (
-                      <DropdownItem key="signup-btn" onPress={onOpenChangeSignUp}>
-                        Registrarse
-                      </DropdownItem>
-                    )
-                  }
-                  {
-                    // Admin Link
-                    (logged && currentUser.role == 'admin') && (
-                      <DropdownItem key="admin-link" href='/admin'>
-                        Admin Dashboard
-                      </DropdownItem>  
-                    )
-                  }
-                  {logged && (
-                    <DropdownItem
-                      key="logout-btn"
-                      onPress={() => {
-                        openModal({
-                          title: `Logout`,
-                          desc: `¿Seguro que deseas salir de la cuenta actual?`,
-                          action: "action",
-                          onSubmit: () => handleLogout(),
-                        });
-                      }}
-                    >
-                      Cerrar Sesión
-                    </DropdownItem>
-                  )}
-                </DropdownSection>
-              </DropdownMenu>
-            </Dropdown>
+            <UserDropdown currentUser={currentUser} logout={logout} onOpenLogin={onOpenLogin} onOpenChangeSignUp={onOpenChangeSignUp}/>
           </NavbarItem>
         </NavbarContent>
       </Navbar>
---
+++ src/components/NavBar/UserDropdown.jsx
@@ -0,0 +1,127 @@
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+} from "@nextui-org/dropdown";
+import { ModalContext } from "@/app/context/ModalContext";
+
+import { User } from "@nextui-org/user";
+import { Button } from "@nextui-org/button";
+import { useGDIcon } from "@/robtop/iconkit/useGDIcon";
+import { useContext } from "react";
+import { apiRequest } from "@/libs/serverRequest";
+import config from "../../../config";
+
+const UserDropdown = ({currentUser, onOpenLogin, onOpenChangeSignUp, logout}) => {
+  const { openModal } = useContext(ModalContext);
+  const handleLogout = async () => {
+    const apiResult = await apiRequest(config.apiURL + "logout");
+    if (apiResult.isError()) {
+      return notify(operationText.error, "error");
+    }
+    logout();
+  };
+
+  // Icon
+  const {icon: iconAvatar} = useGDIcon({
+    type: 'cube',
+    username: currentUser.username,
+    effectDeps: [currentUser.username]
+  })
+
+  const logged = currentUser.username != undefined
+  console.log(currentUser)
+  return (
+    <Dropdown placement="bottom-end">
+      <DropdownTrigger>
+        <Button color="default" variant="flat">
+          {currentUser.username && (<img src={iconAvatar} alt={currentUser.username} className="h-7"/>)}
+          {currentUser.username ?? "Sin cuenta"}
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu
+        aria-label="Profile menu"
+        disabledKeys={["profile"]}
+        className="p-3"
+        itemClasses={{
+          base: [
+            "rounded-md",
+            "text-default-500",
+            "transition-opacity",
+            "data-[hover=true]:text-foreground",
+            "data-[hover=true]:bg-default-100",
+            "dark:data-[hover=true]:bg-default-50",
+            "data-[selectable=true]:focus:bg-default-50",
+            "data-[pressed=true]:opacity-70",
+            "data-[focus-visible=true]:ring-default-500",
+          ],
+        }}
+      >
+        <DropdownSection aria-label="Profile & Actions">
+          <DropdownItem
+            isReadOnly
+            key="profile"
+            className="h-14 gap-2 opacity-100"
+            textValue="User"
+          >
+            <User
+              name={currentUser.username == undefined ? "Invitado" : currentUser.username}
+              description={currentUser.username == undefined ? "none" : currentUser.phone}
+              classNames={{
+                name: "text-default-600",
+                description: "text-default-500",
+              }}
+              avatarProps={{
+                size: "sm",
+                radius: 'none',
+                src: iconAvatar,
+              }}
+            />
+          </DropdownItem>
+          {
+            !logged && (
+              <DropdownItem key="login-btn" onPress={onOpenLogin}>
+                Iniciar Sesión
+              </DropdownItem>
+            ) 
+          }
+          {
+            // Esta doble porque da un error con nextui
+            !logged && (
+              <DropdownItem key="signup-btn" onPress={onOpenChangeSignUp}>
+                Registrarse
+              </DropdownItem>
+            )
+          }
+          {
+            // Admin Link
+            (logged && currentUser.role == 'admin') && (
+              <DropdownItem key="admin-link" href='/admin'>
+                Admin Dashboard
+              </DropdownItem>  
+            )
+          }
+          {logged && (
+            <DropdownItem
+              key="logout-btn"
+              onPress={() => {
+                openModal({
+                  title: `Logout`,
+                  desc: `¿Seguro que deseas salir de la cuenta actual?`,
+                  action: "action",
+                  onSubmit: () => handleLogout(),
+                });
+              }}
+            >
+              Cerrar Sesión
+            </DropdownItem>
+          )}
+        </DropdownSection>
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
+
+export default UserDropdown;

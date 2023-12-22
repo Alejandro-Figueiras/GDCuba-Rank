7b4fea15565ce8b1be52ee835caf8ed08ebf1bb8
Alejandro <alejandrofigueirasx@gmail.com>
2023-12-22 18:20:52 -0500

    Actualiza gameVersion

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ export default {
     // Siempre se debe enviar estos datos a los servidores
     params: {
         secret: 'Wmfd2893gb7',
-        gameVersion: '21',
-        binaryVersion: '35',
+        gameVersion: '22',
+        binaryVersion: '38',
         gdbrowser: '1'
     },
     rateLimiting: true, // Enables rate limiting to avoid api spam, feel free to disable for private use.

d3f4ca68578d10cc2bedc7c4448cc0b8689f4c68
Alejandro <alejandrofigueirasx@gmail.com>
2024-01-13 15:10:02 -0500

    Elimina muchas cosas del Account/Manage

--- src/components/AccountManage/AccountManage.jsx
+++ src/components/AccountManage/AccountManage.jsx
@@ -19,10 +19,6 @@ import { NoAccount } from "../NoAccount";
 import AccountStatsRow from "../Admin/UserModalPanel/AccountStatsRow";
 import GDSpinner from "../GDIcons/GDSpinner";
 import AccountIconsRow from "../Admin/UserModalPanel/AccountIconsRow";
-import { EditIcon } from "../Icons/EditIcon";
-import { AccountGDStuff } from "./AccountGDStuff";
-import ChangeModal from "./ChangeModal";
-import AccountSearchLevel from "./AccountSearchLevel";
 export default function AccountManage() {
   const { currentUser } = useSesion();
   const { icon: iconAvatar } = useGDIcon({
@@ -32,19 +28,6 @@ export default function AccountManage() {
   });
 
   const [fullUser, setFullUser] = useState(undefined);
-  const [changes, setChanges] = useState(new Map());
-
-  const [defaultModalData, setDefaultModalData] = useState({
-    key: undefined,
-    value: "",
-  });
-
-  const { isOpen, onOpenChange, onOpen } = useDisclosure();
-  const {
-    isOpen: isOpenSearch,
-    onOpenChange: onOpenChangeSearch,
-    onOpen: onOpenSearch,
-  } = useDisclosure();
 
   useEffect(() => {
     if (currentUser.username == undefined) return;
@@ -57,55 +40,18 @@ export default function AccountManage() {
     loadFullUser();
   }, [currentUser]);
 
-  const openModal = (key, defaultValue) => {
-    setDefaultModalData({ value: defaultValue, key });
-    onOpen();
-  };
-
-  const openSearchModal = (key, defaultValue) => {
-    setDefaultModalData({ value: defaultValue, key });
-    onOpenSearch();
-  };
-
-  const updateChanges = (key, value) => {
-    if (changes.has(key) && value == currentUser[key]) {
-      changes.delete(key);
-    } else if (value != currentUser[key]) {
-      changes.set(key, value);
-    }
-  };
-
   return currentUser.username != null ? (
     <>
       <Card className="max-w-[1000px] w-[800px]">
         <CardHeader className="flex gap-3">
           <Image
             alt="Cube"
-            height={40}
             radius='none'
             src={iconAvatar}
             width={40}
           />
           <div className="flex flex-col">
-            <p className="text-md">{currentUser.username}</p>
-            <Tooltip content="Frase que verán todos los usuarios al ver tu cuenta">
-              <p
-                className="text-small text-default-500 flex gap-2 justify-center items-center cursor-pointer"
-                onClick={() =>
-                  openModal(
-                    "greeting",
-                    changes.get("greeting") || currentUser.greeting
-                  )
-                }
-              >
-                {currentUser.greeting != null && currentUser.greeting !== ""
-                  ? changes.get("greeting") || currentUser.greeting
-                  : "No tienes ningun saludo definido"}
-                <span>
-                  <EditIcon />
-                </span>
-              </p>
-            </Tooltip>
+            <p className="text-2xl">{currentUser.username}</p>
           </div>
         </CardHeader>
         <Divider />
@@ -114,46 +60,15 @@ export default function AccountManage() {
             <>
               <AccountStatsRow user={fullUser} />
               <AccountIconsRow user={fullUser} />
-              <AccountGDStuff dbuser={currentUser} openModal={openSearchModal} />
-              {/* <AccountComments /> */}
+              
+              
             </>
           ) : (
             <GDSpinner />
           )}
         </CardBody>
         <Divider />
-        <CardFooter className="justify-evenly item-center flex gap-4">
-          <Button
-            color="primary"
-            variant="solid"
-            onPress={() => console.log("aplicar cambios")}
-            className="w-[45%] max-w-200px"
-            isDisabled={changes.size == 0}
-          >
-            Aplicar
-          </Button>
-          <Button
-            color="default"
-            variant="flat"
-            onPress={() => console.log("Revertir cambios")}
-            className="w-[40%] max-w-200px"
-          >
-            Revertir
-          </Button>
-        </CardFooter>
       </Card>
-      <ChangeModal
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        updateChanges={updateChanges}
-        defaultModalData={defaultModalData}
-      />
-      <AccountSearchLevel
-        isOpen={isOpenSearch}
-        onOpenChange={onOpenChangeSearch}
-        updateChanges={updateChanges}
-        defaultModalData={defaultModalData}
-      />
     </>
   ) : (
     <NoAccount />

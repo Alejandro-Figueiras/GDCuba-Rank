46dc0e98be9f9eee43e66ecce841f53e5ace68c9
Alejandro <alejandrofigueirasx@gmail.com>
2024-01-14 20:55:34 -0500

    Agrega submitStuffAction

--- src/actions/admin/stuffActions.js
+++ src/actions/admin/stuffActions.js
@@ -1,7 +1,17 @@
 'use server'
 
-import { getAllStuffItems } from "@/database/db.accstuffitems"
+import { addStuffItem, getAllStuffItems } from "@/database/db.accstuffitems"
+import { authMe } from "../auth/me"
 
 export const getStuffItemsAction = async(props) => {
   return JSON.stringify(await getAllStuffItems(props))
+}
+
+export const submitStuffItemAction = async(props) => {
+  const auth = JSON.parse(await authMe());
+  if (auth.username == props.username && auth.accountID == props.accountid) {
+    return await addStuffItem(props)
+  } else {
+    return -1
+  }
 }
--- src/database/db.accstuffitems.js
+++ src/database/db.accstuffitems.js
@@ -9,3 +9,20 @@ export const getAllStuffItems = async({username, accountid}) => {
   }
   return (result.rowCount)?result.rows:[]
 }
+
+export const addStuffItem = async(item = {}) => {
+  const result = await sql`INSERT INTO accstuffitems(
+    accountid,
+    username,
+    data
+  ) VALUES (
+    ${item.accountid},
+    ${item.username},
+    ${item.data}
+  )`
+  if (result.rowCount) {
+    const result = await sql`SELECT id FROM accstuffitems WHERE accountid=${item.accountid} ORDER BY id DESC LIMIT 1`
+    return result.rows[0]
+  }
+  return -1
+}

b9a9a638a34052299948712579e3ee81b1e62e98
Alejandro <alejandrofigueirasx@gmail.com>
2024-01-30 14:39:46 +0000

    Agrega config de la codespace

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "crispy-space-system-55g7x5p66vp399g-3000.app.github.dev",
+        "localhost:3000"
+      ]
+    }
+  }
+}
 
 module.exports = nextConfig

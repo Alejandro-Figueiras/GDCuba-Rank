c174daf88b67b158a06cd86a6fd3e5493d7eaf24
Alejandro <alejandrofigueirasx@gmail.com>
2024-01-29 22:22:38 -0500

    Agrega Version a la BetaHomePage

---
+++ src/actions/getVersion.js
@@ -0,0 +1,6 @@
+'use server'
+import pjson from '../../package.json'
+
+export const getProjectVersion = async() => {
+  return pjson.version;
+}
--- src/components/Beta/BetaHomePage.jsx
+++ src/components/Beta/BetaHomePage.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import './beta.css'
-
+'use client'
+import { useState, useEffect } from 'react'
+import { getProjectVersion } from '@/actions/getVersion'
 export default function BetaHomePage() {
+
+  const [version, setVersion] = useState(null)
+  useEffect(() => {
+    getProjectVersion().then(setVersion)
+  }, [])
 
   return (
     <div className='m-4 flex flex-col text-center justify-center' style={{
@@ -11,7 +16,8 @@ export default function BetaHomePage() {
         <h1 className='text-3xl font-bold'>Beta Cerrada</h1>
         <p className='text-xl font-semibold mb-4'>Fase 1: Completando Features</p>
         <p className='mb-2'>Recomiendo mucho que no se esfuercen en poner todos los datos y estadisticas reales, sino que para que sea más fácil encontrar bugs, se utilicen datos que no son 100% verídicos. Recuerdo que estamos en una fase de pruebas.</p>
-        <p>En la última fase pre-release se hará un reset de la base de datos y todo lo que hallan hecho será eliminado.</p>
+        <p className='mb-4'>En la última fase pre-release se hará un reset de la base de datos y todo lo que hallan hecho será eliminado.</p>
+        {version && <p className='text-sm font-medium text-default-500'>v{version}</p>}
       </div>
 
     </div>

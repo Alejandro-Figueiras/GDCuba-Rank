51913789e8e6c100d4b0bb0210773938dce18d49
Alejandro <alejandrofigueirasx@gmail.com>
2024-01-31 15:16:51 -0500

    Agrega Ship switcher

--- src/components/Admin/UserModalPanel/AccountIconsRow.jsx
+++ src/components/Admin/UserModalPanel/AccountIconsRow.jsx
@@ -3,6 +3,8 @@ import { useGDIconRef } from "@/robtop/iconkit/useGDIcon";
 import { Card, CardBody } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 
+import './AccountIcons.css'
+
 const AccountIcon = ({
   type = "cube",
   iconNumber = 1,
@@ -41,6 +43,8 @@ const AccountIcon = ({
 };
 
 const AccountIconsRow = ({ user }) => {
+  const [jetpack, setJetpack] = useState(false)
+
   return (
     <Card className="bg-default-200 max-w-full w-full">
       <CardBody className="flex justify-evenly flex-row p-4 flex-wrap">
@@ -53,15 +57,17 @@ const AccountIconsRow = ({ user }) => {
           glow={user.accglow}
           effectDeps={[user]}
         />
-        <AccountIcon
-          type={"ship"}
-          iconNumber={user.accship}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
+        <a href="#" onClick={() => setJetpack(v=>!v)} className="icons__ship">
+          <AccountIcon
+            type={(jetpack)? "jetpack" : "ship"}
+            iconNumber={(jetpack) ? user.accjetpack : user.accship}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user, jetpack]}
+          />
+        </a>
         <AccountIcon
           type={"ball"}
           iconNumber={user.accball}
@@ -110,15 +116,6 @@ const AccountIconsRow = ({ user }) => {
         <AccountIcon
           type={"swing"}
           iconNumber={user.accswing}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
-        <AccountIcon
-          type={"jetpack"}
-          iconNumber={user.accjetpack}
           c1={user.playercolor}
           c2={user.playercolor2}
           c3={user.playercolor3}

a64ed5ef3fa6108a2ae0dc05845956f73a2627cd
Alejandro <alejandrofigueirasx@gmail.com>
2024-01-31 21:59:03 -0500

    Agrega estilos en los iconos

--- src/components/Admin/UserModalPanel/AccountIconsRow.jsx
+++ src/components/Admin/UserModalPanel/AccountIconsRow.jsx
@@ -13,6 +13,8 @@ const AccountIcon = ({
   c3 = 12,
   glow = false,
   effectDeps = [],
+  className = ''
+
 }) => {
   const { icon } = useGDIconRef({
     type,
@@ -29,15 +31,24 @@ const AccountIcon = ({
     setLoading(true);
   }, [icon]);
 
+  const sizeClass = 
+    (type == 'robot')? 'h-12' : 
+    (type == 'wave')? 'h-7' : 
+    (type == 'ufo')? 'h-9' : 
+    (type == 'ship')? 'h-6' : 
+    'h-10'
+
   return (
-    <div className="rounded-md">
-      {loading && <GDSpinner className='h-8 w-8' />}
-      <img
-        ref={icon}
-        alt="Icon"
-        className={`h-10 ${loading && "hidden"}`}
-        onLoad={() => setLoading(false)}
-      />
+    <div className={`flex flex-row justify-center ${className}`}>
+      <div className="h-10 min-w-10 flex flex-col justify-center">
+        {loading && <GDSpinner className='h-8 w-8' />}
+        <img
+          ref={icon}
+          alt="Icon"
+          className={`${sizeClass} w-100 ${loading ? "hidden" : ''}`}
+          onLoad={() => setLoading(false)}
+        />
+      </div>
     </div>
   );
 };
@@ -45,83 +56,142 @@ const AccountIcon = ({
 const AccountIconsRow = ({ user }) => {
   const [jetpack, setJetpack] = useState(false)
 
+  const rowsClassnames = "flex flex-row justify-evenly gap-6 flex-wrap align-middle"
+
   return (
     <Card className="bg-default-200 max-w-full w-full">
-      <CardBody className="flex justify-evenly flex-row p-4 flex-wrap">
-        <AccountIcon
-          type={"cube"}
-          iconNumber={user.accicon}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
-        <a href="#" onClick={() => setJetpack(v=>!v)} className="icons__ship">
+      <CardBody className="p-4">
+        <div className={rowsClassnames}>
           <AccountIcon
-            type={(jetpack)? "jetpack" : "ship"}
-            iconNumber={(jetpack) ? user.accjetpack : user.accship}
+            type={"cube"}
+            iconNumber={user.accicon}
             c1={user.playercolor}
             c2={user.playercolor2}
             c3={user.playercolor3}
             glow={user.accglow}
-            effectDeps={[user, jetpack]}
+            effectDeps={[user]}
           />
-        </a>
+          <a href="#" onClick={() => setJetpack(v=>!v)} className="min-w-11 flex flex-row justify-center icons__ship">
+              <AccountIcon
+                type={"jetpack"}
+                iconNumber={user.accjetpack}
+                c1={user.playercolor}
+                c2={user.playercolor2}
+                c3={user.playercolor3}
+                glow={user.accglow}
+                effectDeps={[user]}
+                className={(jetpack)?'hidden':''}
+              />
+              <AccountIcon
+                type={"ship"}
+                iconNumber={user.accship}
+                c1={user.playercolor}
+                c2={user.playercolor2}
+                c3={user.playercolor3}
+                glow={user.accglow}
+                effectDeps={[user]}
+                className={(!jetpack)?'hidden':''}
+              />
+          </a>
+          <AccountIcon
+            type={"ball"}
+            iconNumber={user.accball}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+          />
+          <AccountIcon
+            type={"ufo"}
+            iconNumber={user.accbird}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+          />
+          <AccountIcon
+            type={"wave"}
+            iconNumber={user.accwave}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+            className="icons__4down-hide"
+          />
+          <AccountIcon
+            type={"robot"}
+            iconNumber={user.accrobot}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+            className="icons__3down-hide"
+          />
+          <AccountIcon
+            type={"spider"}
+            iconNumber={user.accspider}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+            className="icons__3down-hide"
+          />
+          <AccountIcon
+            type={"swing"}
+            iconNumber={user.accswing}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+            className="icons__3down-hide"
+          />
+        </div>
+
+        <div className={`${rowsClassnames} gap-6 justify-center icons_down-row`}>
         <AccountIcon
-          type={"ball"}
-          iconNumber={user.accball}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
-        <AccountIcon
-          type={"ufo"}
-          iconNumber={user.accbird}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
-        <AccountIcon
-          type={"wave"}
-          iconNumber={user.accwave}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
-        <AccountIcon
-          type={"robot"}
-          iconNumber={user.accrobot}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
-        <AccountIcon
-          type={"spider"}
-          iconNumber={user.accspider}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
-        <AccountIcon
-          type={"swing"}
-          iconNumber={user.accswing}
-          c1={user.playercolor}
-          c2={user.playercolor2}
-          c3={user.playercolor3}
-          glow={user.accglow}
-          effectDeps={[user]}
-        />
+            type={"wave"}
+            iconNumber={user.accwave}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+            className="icons__4down-show"
+          />
+          <AccountIcon
+            type={"robot"}
+            iconNumber={user.accrobot}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+          />
+          <AccountIcon
+            type={"spider"}
+            iconNumber={user.accspider}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+          />
+          <AccountIcon
+            type={"swing"}
+            iconNumber={user.accswing}
+            c1={user.playercolor}
+            c2={user.playercolor2}
+            c3={user.playercolor3}
+            glow={user.accglow}
+            effectDeps={[user]}
+          />
+        </div>
       </CardBody>
     </Card>
   );

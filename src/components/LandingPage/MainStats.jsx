c3c863d9e95070d7c85c6aabc44cb95630688fde
Alejandro <alejandrofigueirasx@gmail.com>
2024-03-25 20:56:13 -0400

    Mejora estilos de MainStats

--- src/components/LandingPage/MainStats.jsx
+++ src/components/LandingPage/MainStats.jsx
@@ -1,42 +1,47 @@
+'use client'
 import StatsCard from "./StatsCard"
+import { useState, useEffect } from 'react'
 
 const MainStats = ({}) => {
+  const [landingStats, setLandingStats] = useState({
+    totalStars: 0,
+    totalDemons: 0,
+    totalUsercoins: 0,
+    totalCreatorPoints: 0,
+    totalMoons: 0,
+    hardest: {}
+  });
+
   const cards = [
     {
       id: 'stars',
       title: "Total de Estrellas Acumuladas",
-      value: "30000000",
+      value: landingStats.totalStars,
       img: '/assets/stats/starsIcon.png'
     },
     {
-      id: 'stars',
-      title: "Total de Estrellas Acumuladas",
-      value: "30000000",
-      img: '/assets/stats/starsIcon.png'
+      id: 'demons',
+      title: "Total de Demons Acumulados",
+      value: landingStats.totalDemons,
+      img: '/assets/dificultades/none/hard_demon.png'
     },
     {
-      id: 'stars',
-      title: "Total de Estrellas Acumuladas",
-      value: "30000000",
-      img: '/assets/stats/starsIcon.png'
+      id: 'moons',
+      title: "Total de Lunas Acumuladas",
+      value: landingStats.totalMoons,
+      img: '/assets/stats/moonsIcon.png'
     },
     {
-      id: 'stars',
-      title: "Total de Estrellas Acumuladas",
-      value: "30000000",
-      img: '/assets/stats/starsIcon.png'
+      id: 'usercoins',
+      title: "Total de Coins Acumuladas",
+      value: landingStats.totalUsercoins,
+      img: '/assets/stats/usercoin.png'
     },
     {
-      id: 'stars',
-      title: "Total de Estrellas Acumuladas",
-      value: "30000000",
-      img: '/assets/stats/starsIcon.png'
-    },
-    {
-      id: 'stars',
-      title: "Total de Estrellas Acumuladas",
-      value: "30000000",
-      img: '/assets/stats/starsIcon.png'
+      id: 'cp',
+      title: "Total de Creator Points Acumulados",
+      value: landingStats.totalCreatorPoints,
+      img: '/assets/stats/creatorpoints.png'
     },
   ]
 
--- src/components/LandingPage/StatsCard.jsx
+++ src/components/LandingPage/StatsCard.jsx
@@ -1,7 +1,7 @@
 const StatsCard = ({ id, title, value, img }) => {
   return <div class="b border-1 rounded-md border-default-500 [background-color:#00000099] p-8 w-96">
     <div class="flex flex-col items-center">
-      <img src={img} alt={`Imagen ${id}`} class="w-42"/>
+      <img src={img} alt={`Imagen ${id}`} class="h-20"/>
       <p class="text-lg font-semibold">{title}</p>
       <p class="text-2xl font-bold">{value}</p>
     </div>

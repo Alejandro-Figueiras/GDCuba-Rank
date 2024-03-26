635b038ec565c7aa1462c18bc3ce0f03455fd238
Alejandro <alejandrofigueirasx@gmail.com>
2024-03-25 21:11:10 -0400

    Aumenta border radius del StatsCard

--- src/components/LandingPage/StatsCard.jsx
+++ src/components/LandingPage/StatsCard.jsx
@@ -1,5 +1,5 @@
 const StatsCard = ({ id, title, value, img }) => {
-  return <div class="border-1 rounded-md border-default-500 [background-color:#00000099] p-8 w-96">
+  return <div class="border-1 rounded-xl border-default-500 [background-color:#00000099] p-8 w-96">
     <div class="flex flex-col items-center">
       <img src={img} alt={`Imagen ${id}`} class="h-20"/>
       <p class="text-lg font-semibold">{title}</p>

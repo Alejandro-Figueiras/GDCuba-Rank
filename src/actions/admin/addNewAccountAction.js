f638736db2b90225a21e9be9c6218c8bd8dfa4b3
Alejandro <alejandrofigueirasx@gmail.com>
2024-03-25 22:00:59 -0400

    Agrega autoupdate de landing stats

--- src/actions/admin/addNewAccountAction.js
+++ src/actions/admin/addNewAccountAction.js
@@ -1,9 +1,12 @@
 'use server'
 import { addGDAccount } from "@/database/db.gdaccounts"
+import { updateLandingStatsAcc } from "@/database/db.staticInfo"
 import { authorize } from "@/libs/secure"
 
 export const addNewAccountAction = async({account, cuba = 0}) => {
   if (account && await authorize()) {
-    return JSON.stringify(await addGDAccount({account, cuba}))
+    const result = JSON.stringify(await addGDAccount({account, cuba}))
+    updateLandingStatsAcc();
+    return result
   }
 }
--- src/actions/admin/recordLevelsAction.js
+++ src/actions/admin/recordLevelsAction.js
@@ -1,6 +1,7 @@
 'use server'
 
 import { getAllLevelsByDifficulty, reposicionarNivel } from "@/database/db.records";
+import { updateLandingStatsRecords } from "@/database/db.staticInfo";
 import { authorize } from "@/libs/secure"
 
 export const getAllLevelsByDifficultyAction = async({difficulty = 15}) => {
@@ -12,7 +13,9 @@ export const getAllLevelsByDifficultyAction = async({difficulty = 15}) => {
 
 export const reposicionarLevelAction = async({levelid, oldScore, newScore}) => {
   if (await authorize()) {
-    return await reposicionarNivel(levelid, oldScore, newScore)
+    const result = await reposicionarNivel(levelid, oldScore, newScore)
+    updateLandingStatsRecords()
+    return result
   }
   return 0;
 }

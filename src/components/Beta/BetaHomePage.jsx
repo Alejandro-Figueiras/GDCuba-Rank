e828554bb412bab4722ff5d45ca0162792c232d0
Alejandro <alejandrofigueirasx@gmail.com>
2024-03-29 15:05:00 -0400

    Beta 0.2.0

--- src/components/Beta/BetaHomePage.jsx
+++ src/components/Beta/BetaHomePage.jsx
@@ -12,7 +12,8 @@ export default function BetaHomePage() {
     <div className='m-4 flex flex-col text-center justify-center'>
       <div className='container max-w-[800px] mx-auto'>
         <h1 className='text-3xl font-bold'>Beta Cerrada</h1>
-        <p className='text-xl font-semibold mb-4'>Fase 1: Completando Features</p>
+        <p className='text-xl font-semibold mb-4'>Fase 2: Bugfix</p>
+        <p className='mb-2'>En esta fase su objetivo es romper la página tanto visual como funcionalmente. Cuando encuentren un bug, necesito que lo identifiquen lo mejor que puedan, y si es algo de alguna funcionalidad, que expliquen como provocarlo, en mi caso es mucho más sencillo arreglarlo.</p>
         <p className='mb-2'>Recomiendo mucho que no se esfuercen en poner todos los datos y estadisticas reales, sino que para que sea más fácil encontrar bugs, se utilicen datos que no son 100% verídicos. Recuerdo que estamos en una fase de pruebas.</p>
         <p className='mb-4'>En la última fase pre-release se hará un reset de la base de datos y todo lo que hallan hecho será eliminado.</p>
         {version && <p className='text-sm font-medium text-default-500'>v{version}</p>}

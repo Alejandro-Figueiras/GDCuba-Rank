7549674eaa04e11bf3d850ce939eda8e4ddb6f11
Alejandro <alejandrofigueirasx@gmail.com>
2024-04-06 21:20:34 -0400

    Arregla responsive design en Admin TablaHeader

--- src/components/Admin/TablaHeader.jsx
+++ src/components/Admin/TablaHeader.jsx
@@ -4,8 +4,8 @@ import { Button } from "@nextui-org/react"
 const TablaHeader = ({title, buttons = [], children}) => {
   return (
     <div className="px-8 py-4">
-      <div className="flex justify-between">
-        <h2 className="pt-4 pb-2 text-2xl">{title}</h2>
+      <div className="flex items-center flex-col sm:flex-row sm:justify-between pb-4">
+        <h2 className="pb-2 text-2xl">{title}</h2>
         <div className="flex gap-2">
           {buttons.map((button, i) => 
           <Button onClick={button.handleClick} key={i}>

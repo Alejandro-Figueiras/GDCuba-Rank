64e4ae23afdf36df718b8429602676ac14024829
Alejandro <alejandrofigueirasx@gmail.com>
2024-04-06 21:22:34 -0400

    Arregla bug TablaHeader title center

--- src/components/Admin/TablaHeader.jsx
+++ src/components/Admin/TablaHeader.jsx
@@ -5,7 +5,7 @@ const TablaHeader = ({title, buttons = [], children}) => {
   return (
     <div className="px-8 py-4">
       <div className="flex items-center flex-col sm:flex-row sm:justify-between pb-4">
-        <h2 className="pb-2 text-2xl">{title}</h2>
+        <h2 className="pb-2 text-2xl text-center sm:text-start">{title}</h2>
         <div className="flex gap-2">
           {buttons.map((button, i) => 
           <Button onClick={button.handleClick} key={i}>

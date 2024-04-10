40d8e1a159f986148494d5c871c182b067a97c38
Alejandro <alejandrofigueirasx@gmail.com>
2024-04-10 18:11:27 +0000

    Elimina BetaHomePage

--- src/app/(front)/page.jsx
+++ src/app/(front)/page.jsx
@@ -8,13 +8,9 @@ import MainFooter from "@/components/LandingPage/MainFooter";
 export default function Home() {
   return (
     <> 
-      <div className="absolute top-0 left-0 -z-10 w-full opacity-50 flex flex-row justify-center global-full-viewport landing-background">
-      </div>
+      <div className="absolute top-0 left-0 -z-10 w-full opacity-50 flex flex-row justify-center global-full-viewport landing-background"></div>
       <MainHero/>
       <MainStats />
-      <div>
-        <BetaHomePage />
-      </div>
       <MainFooter />
     </>
   );

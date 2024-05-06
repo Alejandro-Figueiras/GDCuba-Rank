6ada29cdb57dff79132d24758b14f8defd34bb3c
Alejandro <alejandrofigueirasx@gmail.com>
2024-05-06 14:02:25 -0400

    Migra db.accstuffitems a typescript

--- src/database/db.accstuffitems.ts
+++ src/database/db.accstuffitems.ts
@@ -1,7 +1,14 @@
+import DictionaryObject from '@/helpers/DictionaryObject'
 import { sql } from '@vercel/postgres'
 import { unstable_noStore as noStore } from 'next/cache'
 
-export const getAllStuffItems = async ({ username, accountid }) => {
+export const getAllStuffItems = async ({
+  username,
+  accountid
+}: {
+  username?: string
+  accountid?: number
+}) => {
   noStore()
   let result
   if (accountid) {
@@ -13,7 +20,11 @@ export const getAllStuffItems = async ({ username, accountid }) => {
   return result.rowCount ? result.rows : []
 }
 
-export const addStuffItem = async (item = {}) => {
+export const addStuffItem = async (item: {
+  accountid: number
+  username: string
+  data: string
+}) => {
   noStore()
   const result = await sql`INSERT INTO accstuffitems(
     accountid,
@@ -32,27 +43,33 @@ export const addStuffItem = async (item = {}) => {
   return -1
 }
 
-export const updateStuffItemData = async (id, data) => {
+export const updateStuffItemData = async (id: number, data: string) => {
   noStore()
   const result =
     await sql`UPDATE accstuffitems SET data = ${data} WHERE id = ${id}`
   return result.rowCount ? 1 : 0
 }
 
-export const deleteStuffItem = async (id) => {
+export const deleteStuffItem = async (id: number) => {
   noStore()
   const result = await sql`DELETE FROM accstuffitems WHERE id = ${id}`
   return result.rowCount ? 1 : 0
 }
 
-export const deleteStuffItemsByUsername = async (username) => {
+export const deleteStuffItemsByUsername = async (username: string) => {
   noStore()
   const result =
     await sql`DELETE FROM accstuffitems WHERE username = ${username}`
   return result.rowCount ? 1 : 0
 }
 
-export const renameUserInStuffItems = async ({ accountid, username }) => {
+export const renameUserInStuffItems = async ({
+  accountid,
+  username
+}: {
+  accountid: number
+  username: string
+}) => {
   noStore()
   return (
     await sql`UPDATE accstuffitems SET username = ${username} WHERE accountid = ${accountid}`

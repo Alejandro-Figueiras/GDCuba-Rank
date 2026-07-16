5861b297c2ac94d45d09c9bf36f65ecbfaaff342
Alejandro <alejandrofigueirasx@gmail.com>
2026-07-15 21:32:58 -0400

    fix: middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,7 +5,7 @@ import { COOKIES_INFO } from './models/constants'
 import { jwtVerify } from 'jose'
 // This function can be marked `async` if using `await` inside
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const cookieStore = await cookies()
   const cookie = cookieStore.get(COOKIES_INFO.name)
   const cookieData = cookie ? cookie.value : undefined

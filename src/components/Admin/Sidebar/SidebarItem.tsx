02ff69c98d529e965f298b00cf0cff9bbfa5173b
Alejandro <alejandrofigueirasx@gmail.com>
2026-07-22 18:14:05 -0400

    fix: sidebar item styles

--- src/components/Admin/Sidebar/SidebarItem.tsx
+++ src/components/Admin/Sidebar/SidebarItem.tsx
@@ -23,13 +23,13 @@ export const SidebarItem = ({
 
   const clases =
     (isActive
-      ? 'bg-primary-100 [&_svg_path]:fill-primary-500'
+      ? 'bg-accent/40 [&_svg_path]:fill-accent'
       : 'hover:bg-default-100') +
     ' ' +
     'flex gap-2 w-full min-h-[44px] h-full items-center px-3.5 rounded-xl cursor-pointer transition-all duration-150 active:scale-[0.98]'
 
   return (
-    <Link href={href} className='max-w-full text-default-900 active:bg-none'>
+    <Link href={href} className='text-default-900 max-w-full active:bg-none'>
       <div className={clases} onClick={handleClick}>
         {icon}
         <span className='text-default-900'>{title}</span>
